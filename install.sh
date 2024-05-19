03479cfd4c8924cbdf7658b1f5487daec98854ed
riviox <riviox13@gmail.com>
2024-05-19 19:09:47 +0200

    fix

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 cd files
+git submodule update --init --recursive
 sudo make dmks_remove
 sudo make dkms_install
 sudo make && sudo make install
+echo "Reboot? [y/N] "
+read yn
+[[ yn == "y" ]] && reboot
+[[ yn == "Y" ]] && reboot
